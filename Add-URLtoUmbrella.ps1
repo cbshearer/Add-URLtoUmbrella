49b86e041ba22ffb5fe63360de7e24a41abae366
Chris Shearer <cbshearer@gmail.com>
2019-08-30 13:23:51 -0700

    Update Add-URLtoUmbrella.ps1
    Working, JSON fields case sensitive.

--- Add-URLtoUmbrella.ps1
+++ Add-URLtoUmbrella.ps1
@@ -1,34 +1,47 @@
 ## Umbrella Enforcement API: https://docs.umbrella.com/enforcement-api/reference/
 
-## specify TLS1.2
+## specify TLS
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12 
 
-## PP TAP PS key
-$UmbrellaCustomerKey    = "1111-2222-3333-4444"
-$UmbrellaURI            = "https://s-platform.api.opendns.com/1.0/events?customerKey=" + $UmbrellaCustomerKey
-write-host "$UmbrellaURI" -f Green
+## Umbrella key
+    $UmbrellaCustomerKey    = "1111-2222-3333-4444"
+    $UmbrellaURI            = "https://s-platform.api.opendns.com/1.0/events?customerKey=" + $UmbrellaCustomerKey
 
-    ## Specify headers
-        $UmbrellaHeaders = @{'Content-Type' = 'application/json'}
+## Domains to add    
+    $Domain = "internetbadguys.com"
+    $MalURL = "https://www.internetbadguys.com/bad/url.php"
 
-    ## build date to specification
-        $UmbrellaDate = get-date -f yyyy-mm-ddThh:mm:ssZ
+## Specify headers
+    $UmbrellaHeaders = @{'Content-Type' = 'application/json'}
+
+## build date to specification
+    $UmbrellaDate = get-date -f yyyy-MM-ddThh:mm:ssZ
     
-    ## build body 
-        $UmbrellaBody = @("`{`"alertTime`":`"$UmbrellaDate`";`"deviceID`":`"WXYZ`";`"deviceVersion`":`"TAP via Powershell 1.0`";`"dstDomain`":`"internetbadguys.com`";`"dstURL`":`"https://internetbadguys.com/bad/file/path`";`"eventTime`":`"$UmbrellaDate`";`"protocolVersion`":`"1.0a`";`"providerName`":`"Security Platform`"`}")
+## build body 
+    $UmbrellaBody =  [pscustomobject]@{"alertTime"=$UmbrellaDate;
+                                        "deviceId"=$env:computername;
+                                        "deviceVersion"="TAP via Powershell 1.0";
+                                        "dstDomain"=$domain;
+                                        "dstUrl"=$MalURL;
+                                        "eventTime"=$UmbrellaDate;
+                                        "protocolVersion"="1.0a";
+                                        "providerName"="Security Platform";
+                                        }
         
-    ## convert body to JSON?
-        $UmbrellaBody = $UmbrellaBody | ConvertTo-Json
+## convert body to JSON?
+    $UmbrellaBody = $UmbrellaBody | ConvertTo-Json
 
-    ## Add this domain/url to Umbrella            
-        try { 
-            ## make the call
-                $UmbrellaAdd = Invoke-RestMethod -uri $UmbrellaURI -ErrorVariable RestError -Headers $UmbrellaHeaders -body $UmbrellaBody -Method Post
-            }
-        catch {
-                $catch = $_.Exception ## if there is an error store it as catch
-                write-host -f cyan $catch
-                write-host -f blue $RestError
-            }
-    ## resulting data stored here
-        $UmbrellaAdd
+## Add this domain/url to Umbrella            
+    try { 
+        ## make the call
+            $UmbrellaAdd = Invoke-RestMethod -uri $UmbrellaURI -ErrorVariable RestError -Headers $UmbrellaHeaders -body $UmbrellaBody -Method Post
+        }
+    catch {
+            $cat = $_.Exception ## if there is an error store it as catch
+            $dog = $_
+            write-host -f cyan $cat
+            write-host -f blue $RestError
+            write-host -f Magenta $dog
+        }
+## resulting data stored here
+    if ($UmbrellaAdd.id) {write-host "Success: " $umbrellaAdd.id}
