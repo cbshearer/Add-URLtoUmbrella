b805aba58d0988e0ad7476e1a482968253180ae6
Chris Shearer <cbshearer@gmail.com>
2019-09-03 19:38:49 -0700

    Cleanup

--- Add-URLtoUmbrella.ps1
+++ Add-URLtoUmbrella.ps1
@@ -3,17 +3,17 @@
 ## 8.30.2019
 ## Umbrella Enforcement API: https://docs.umbrella.com/enforcement-api/reference/
 
-## specify TLS
+## Specify TLS
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12 
 
 ## Umbrella key
     $UmbrellaCustomerKey    = "1111-2222-3333-4444"
     $UmbrellaURI            = "https://s-platform.api.opendns.com/1.0/events?customerKey=" + $UmbrellaCustomerKey
 
-## Domains to add    
+## Domain to add    
     $MalURL = "https://www.internetbadguys.com/bad/url.php"
 
-## clean up URL
+## Clean up URL
     ## add http:// if it isn't there
         if  ($malurl -notlike "http*") {$malurl = "http://" + $malurl}
     ## Reduce full URL to just the domain
@@ -21,13 +21,13 @@
         write-host "Domain : " $Domain
         if (!($domain)) {$domain = $malurl}
 
-## Specify headers
+## Specify header
     $UmbrellaHeaders = @{'Content-Type' = 'application/json'}
 
-## build date to specification
+## Compose date to specification
     $UmbrellaDate = get-date -f yyyy-MM-ddThh:mm:ssZ
     
-## build body 
+## uild body 
     $UmbrellaBody =  [pscustomobject]@{"alertTime"=$UmbrellaDate;
                                         "deviceId"=$env:computername;
                                         "deviceVersion"="TAP via Powershell 1.0";
@@ -38,7 +38,7 @@
                                         "providerName"="Security Platform";
                                         }
         
-## convert body to JSON?
+## Convert body to JSON
     $UmbrellaBody = $UmbrellaBody | ConvertTo-Json
 
 ## Add this domain/url to Umbrella            
@@ -47,11 +47,9 @@
             $UmbrellaAdd = Invoke-RestMethod -uri $UmbrellaURI -ErrorVariable RestError -Headers $UmbrellaHeaders -body $UmbrellaBody -Method Post
         }
     catch {
-            $cat = $_.Exception ## if there is an error store it as catch
-            $dog = $_
+            $cat = $_                      ## If there is an error store it as $cat
             write-host -f cyan $cat
             write-host -f blue $RestError
-            write-host -f Magenta $dog
         }
-## resulting data stored here
+## Resulting data stored here
     if ($UmbrellaAdd.id) {write-host -f Green "Success: " $umbrellaAdd.id}
