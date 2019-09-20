5dc7b3c36959adbb7d2d973d53a9a7a20e74a972
Chris Shearer <cbshearer@gmail.com>
2019-09-20 09:19:13 -0700

    Update Add-URLtoUmbrella.ps1
    Loop through an array of URLs instead of a single.

--- Add-URLtoUmbrella.ps1
+++ Add-URLtoUmbrella.ps1
@@ -3,53 +3,80 @@
 ## 8.30.2019
 ## Umbrella Enforcement API: https://docs.umbrella.com/enforcement-api/reference/
 
-## Specify TLS
+## specify TLS
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
 
-## Umbrella key
-    $UmbrellaCustomerKey    = "1111-2222-3333-4444"
-    $UmbrellaURI            = "https://s-platform.api.opendns.com/1.0/events?customerKey=" + $UmbrellaCustomerKey
+## Umbrella Key
+    $CredFile   = import-csv 'E:\scripts\powershell\TAP\credentials.csv'
+    $UmbrellaCustomerKey   = $CredFile | Where-Object -Property Type -eq 'UmbrellaCustomerKey'
+    $UmbrellaCustomerKey   = $UmbrellaCustomerKey.data
+    $UmbrellaURI = "https://s-platform.api.opendns.com/1.0/events?customerKey=" + $UmbrellaCustomerKey
 
-## Domain to add    
-    $MalURL = "https://www.internetbadguys.com/bad/url.php"
+## Array of URLs to parse and add
+    $MalURLs = @()
+    $MalURLs += "https://www.internetbadguys.com/bad/url.php"
+    $MalURLs += "http://exampleadultsite.com"
 
-## Clean up URL
-    ## add http:// if it isn't there
-        if  ($malurl -notlike "http*") {$malurl = "http://" + $malurl}
-    ## Reduce full URL to just the domain
-        $Domain = ([System.Uri]$malURL).Host -replace '^www\.'
-        write-host "Domain : " $Domain
-        if (!($domain)) {$domain = $malurl}
+## Reference an external customizable whitelist file rather than list multiple in this script. 
+## Note the Umbrella 'Domain Acceptance Process' already sorts the domains and compares against the Alexa top 1000 (https://docs.umbrella.com/enforcement-api/reference/#domain-acceptance-process-1)
+    $whitelist = get-content 'E:\scripts\powershell\add-urltoUmbrella\whitelist.txt'
 
-## Specify header
-    $UmbrellaHeaders = @{'Content-Type' = 'application/json'}
+## Loop through each url in the array.
+foreach ($MalURL in $MalURLs)
+    {
+        $UmbrellaAdd = $null
+        write-host -f cyan "==================="
 
-## Compose date to specification
-    $UmbrellaDate = get-date -f yyyy-MM-ddThh:mm:ssZ
-    
-## Build body 
-    $UmbrellaBody =  [pscustomobject]@{"alertTime"=$UmbrellaDate;
-                                        "deviceId"=$env:computername;
-                                        "deviceVersion"="TAP via Powershell 1.0";
-                                        "dstDomain"=$domain;
-                                        "dstUrl"=$MalURL;
-                                        "eventTime"=$UmbrellaDate;
-                                        "protocolVersion"="1.0a";
-                                        "providerName"="Security Platform";
-                                        }
+        ## Clean up URL
+            ## Add http:// if it isn't there
+                if  ($MalURL -notlike "http*") {$MalURL = "http://" + $MalURL}
+            ## Reduce full URL to just the domain
+                $domain = ([System.Uri]$MalURL).Host -replace '^www\.'
+                write-host "Domain : " $domain
+                write-host "Url    : " $MalURL
+                if (!($domain)) {$domain = $MalURL}
         
-## Convert body to JSON
-    $UmbrellaBody = $UmbrellaBody | ConvertTo-Json
+        ## Check whitelist before blocking
+            if ($whitelist -notcontains $domain)                                
+                {
+                    write-host $domain -nonewline; write-host -f Green " not found in whitelist"
+                    write-host $UmbrellaURI
+                    ## Specify headers
+                        $UmbrellaHeaders = @{'Content-Type' = 'application/json'}
+                    
+                    ## build date to specification
+                        $UmbrellaDate = get-date -f yyyy-MM-ddThh:mm:ssZ
+                    
+                    ## build body 
+                        $UmbrellaBody =  [pscustomobject]@{ "alertTime"=$UmbrellaDate;
+                                                            "deviceId"=$env:computername;
+                                                            "deviceVersion"="Add-URLtoUmbrella 1.0";
+                                                            "dstDomain"=$domain;
+                                                            "dstUrl"=$MalURL;
+                                                            "eventTime"=$UmbrellaDate;
+                                                            "protocolVersion"="1.0a";
+                                                            "providerName"="Security Platform";
+                                                        }
+                    ## convert body to JSON?
+                        $UmbrellaBody = $UmbrellaBody | ConvertTo-Json
 
-## Add this domain/url to Umbrella            
-    try { 
-        ## make the call
-            $UmbrellaAdd = Invoke-RestMethod -uri $UmbrellaURI -ErrorVariable RestError -Headers $UmbrellaHeaders -body $UmbrellaBody -Method Post
-        }
-    catch {
-            $cat = $_                      ## If there is an error store it as $cat
-            write-host -f cyan $cat
-            write-host -f blue $RestError
-        }
-## Resulting data stored here
-    if ($UmbrellaAdd.id) {write-host -f Green "Success: " $umbrellaAdd.id}
+                    ## Add this domain/url to Umbrella
+                        try { 
+                            Start-Sleep 2 ## slow it down so we dont hit the rate limit of 100 calls/min.
+                            $UmbrellaAdd = Invoke-RestMethod -uri $UmbrellaURI -ErrorVariable RestError -Headers $UmbrellaHeaders -body $UmbrellaBody -Method Post 
+                        }
+                        catch {
+                            $cat = $_.Exception ## if there is an error store it as catch
+                            $dog = $_
+                            write-host -f cyan $cat
+                            write-host -f blue $RestError
+                            write-host -f Magenta $dog
+                        }
+                    ## resulting data stored here
+                        if ($UmbrellaAdd.id) {
+                            write-host -f Green "Success:" $umbrellaAdd.id
+                            Write-Host -f Darkcyan $MalURL
+                        }
+                }
+            else {Write-Host "The domain " -NoNewline; Write-Host -f green $MalURL -NoNewline; write-host " was found in the whitelist."}
+    }
